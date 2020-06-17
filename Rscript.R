42e02b20d231f8f60f19d9172e2fdd8ac3575ca7
Kushani Semasinghe <kushanisemasinghe@gmail.com>
2020-06-17 09:31:05 +0530

    restored

--- Rscript.R
+++ Rscript.R
@@ -1,4 +1,23 @@
+# This is a R script
 
-shggsdh
-jhsvvdhj
+x<-c(1,2,3,4,5)
+mean(x)
+
+y<-c(6,7,8,9,10)
+mean(y)
+sd(y)
+
+z<-c(1,4,5,7,9,10,11,12,13)
+mean(z)
+.libPaths()
+
+k<-axc
+
+
+gsvhs
+jgsvhxs
+jhsvjs
+
+
+x<-c(1,2,4,5)
 
