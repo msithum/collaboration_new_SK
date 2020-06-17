4eeedebdb33e8fb3cf73e298631eb77339d6f2c4
Sithum Munasinghe <90938917@westernsydney.edu.au>
2020-06-17 13:59:20 +1000

    dropped

--- Rscript.R
+++ Rscript.R
@@ -1,23 +1,4 @@
-# This is a R script
 
-x<-c(1,2,3,4,5)
-mean(x)
-
-y<-c(6,7,8,9,10)
-mean(y)
-sd(y)
-
-z<-c(1,4,5,7,9,10,11,12,13)
-mean(z)
-.libPaths()
-
-k<-axc
-
-
-gsvhs
-jgsvhxs
-jhsvjs
-
-
-x<-c(1,2,4,5)
+shggsdh
+jhsvvdhj
 
